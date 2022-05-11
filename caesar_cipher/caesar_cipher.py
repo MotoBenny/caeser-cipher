c04694135f8af3abcf0da2bcc3086d3b5dff4ce3
Benjamin Carter <benj.james.carter@gmail.com>
2022-05-11 15:45:20 -0700

    encrypt + decrypt passing

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,11 +1,33 @@
 
-
-def encrypt(string,shift):
-    return 'bqqmf'
+abc = 'abcdefghijklmnopqrstuvwxyz'
 
 
-def decrypt(encrypted,shift):
-    pass
+def encrypt(message, shift):
+    encrypted = ""
+
+    if shift < 0:
+        shift = shift % 26
+    for char in message:
+        if not char.isalpha():
+            encrypted += char
+        else:
+            index = abc.find(char.lower())
+            index += shift
+            if index >= len(abc):
+                index -= len(abc)
+            elif index < 0:
+                index += len(abc)
+            if char.isupper():
+                letter = abc[index]
+                encrypted += letter.upper()
+            if char.islower():
+                encrypted += abc[index]
+
+    return encrypted
+
+
+def decrypt(encrypted, shift):
+    return encrypt(encrypted, -shift)
 
 
 def crack(encrypted):
